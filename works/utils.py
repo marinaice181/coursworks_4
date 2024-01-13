00b266742886c42f9d714ef7c8eae681165dc7c9
Marina <anfina1@yandex.ru>
2024-01-13 20:52:15 +0300

    доработки и добавления

--- works/utils.py
+++ works/utils.py
@@ -4,12 +4,69 @@ from works.jsonsaver import JSONSaver
 
 
 def user_hh():
-    pass
+    keyword = input('Напишите профессию, которую вы ищите: \n')
+    hh_api = HeadHunterAPI()
+    print('Сколько вывести страниц? \n')
+    pages = int(input())
+    from_hh = hh_api.get_vacancies(keyword, pages)
+    print('Список вакансий с сайта "HeadHuter": \n')
+    for i in from_hh:
+        print(i)
+    print('Записать отсортированные по зарплате данные в JSON файл? \n')
+    user_answer = input('Да\Нет \n')
+    if user_answer not in ['да']:
+        print('Спасибо за использование программы!!')
+    else:
+        jsonfile_hh = JSONSaver()
+        jsonfile_hh.add_vacancies(from_hh)
+        jsonfile_hh.sort_vacancies_by_salary()
+        jsonfile_hh.file_writer()
+        return jsonfile_hh
 
 
 def user_sj():
-    pass
+    keyword = input('Напишите профессию, которую вы ищите: \n')
+    superjob_api = SuperJobAPI()
+    print('Сколько вывести страниц? \n')
+    pages = int(input())
+    from_sj = superjob_api.get_vacancies(keyword, pages)
+    print('Список вакансий с сайта "SuperJob": \n')
+    for i in from_sj:
+        print(i)
+    print('Записать отсортированные по зарплате данные в JSON файл? \n')
+    user_answer = input('Да\Нет \n')
+    if user_answer not in ['да']:
+        print('Спасибо за использование программы!')
+    else:
+        jsonfile_sj = JSONSaver()
+        jsonfile_sj.add_vacancies(from_sj)
+        jsonfile_sj.sort_vacancies_by_salary()
+        jsonfile_sj.file_writer()
+        return jsonfile_sj
 
 
 def user_choice_both():
-    pass
+    keyword = input('Напишите профессию, которую вы ищите: \n')
+    hh_api = HeadHunterAPI()
+    superjob_api = SuperJobAPI()
+    print('Сколько вывести страниц? \n')
+    pages = int(input())
+    from_hh = hh_api.get_vacancies(keyword, pages)
+    from_sj = superjob_api.get_vacancies(keyword, pages)
+    print('Список вакансий с сайта "HeadHuter":\n')
+    for i in from_hh:
+        print(i)
+    print('Список вакансий с сайта "SuperJob":\n')
+    for i in from_sj:
+        print(i)
+    print('Записать отсортированные по зарплате данные в JSON файл? \n')
+    user_answer = input('Да\Нет \n').lower()
+    if user_answer not in ['да']:
+        print('Спасибо за использование программы! \n')
+    else:
+        jsonfile_all = JSONSaver()
+        jsonfile_all.add_vacancies(from_hh)
+        jsonfile_all.add_vacancies(from_sj)
+        jsonfile_all.sort_vacancies_by_salary()
+        jsonfile_all.file_writer()
+        return jsonfile_all
--- works/vacancy.py
+++ works/vacancy.py
@@ -53,15 +53,28 @@ class Vacancy:
         """
         Возвращает вакансию в виде словаря
         """
-        pass
+        return {
+            'vacancy_title': self.vacancy_title,
+            'town': self.town,
+            'salary_from': self.salary_from,
+            'salary_to': self.salary_to,
+            'employment': self.employment,
+            'url': self.url
+        }
 
     @staticmethod
     def from_dict(vacancy_dict):
         """
         Возвращает вакансию в виде списка
         """
-        pass
-
+        return Vacancy(
+            vacancy_dict['vacancy_title'],
+            vacancy_dict['town'],
+            vacancy_dict['salary_from'],
+            vacancy_dict['salary_to'],
+            vacancy_dict['employment'],
+            vacancy_dict['url']
+        )
 
 
 class Vacancies:
@@ -71,3 +84,23 @@ class Vacancies:
 
     def __init__(self):
         self.__all_vacancies = []
+
+    def add_vacancies(self, new_vacancies):
+        self.__all_vacancies += new_vacancies
+
+    def delete_vacancies(self, old_vacancies):
+        for i in old_vacancies:
+            self.__all_vacancies.remove(i)
+
+    def sort_vacancies_by_salary(self):
+        self.__all_vacancies.sort(reverse=True)
+
+    @property
+    def all_vacancies(self):
+        return self.__all_vacancies
+
+    def to_list_dict(self):
+        a = []
+        for i in self.__all_vacancies:
+            a.append(i.to_dict())
+        return a
